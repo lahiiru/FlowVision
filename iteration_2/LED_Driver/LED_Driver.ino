db7572553c4fcf15363c5e7a8bdae2bd7495bbc0
JA Lahiru P Jayakody <lahiiru@gmail.com>
2017-09-26 10:27:22 +0530

    LED driver Arduino code added

---
+++ iteration_2/LED_Driver/LED_Driver.ino
@@ -0,0 +1,56 @@
+#define red_pin 13
+#define blue_pin 12
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(red_pin, OUTPUT);
+  pinMode(blue_pin, OUTPUT);
+  Serial.begin(115200);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // PulseOne();
+  PulseTwo();
+  // Sync();
+}
+
+void PulseOne(){
+  digitalWrite(red_pin, HIGH);
+  delay(1);
+  digitalWrite(red_pin, LOW);
+  delay(1000);
+}
+void Sync(){
+  if(Serial.available()){
+    char c = Serial.read();
+    switch(c){
+      case 'R':
+        digitalWrite(red_pin, HIGH); break;
+      case 'r':
+        digitalWrite(red_pin, LOW); break;
+      case 'B':
+        digitalWrite(blue_pin, HIGH); break;
+      case 'b':
+        digitalWrite(blue_pin, LOW); break;
+    }
+  }
+}
+void PulseTwo(){
+  digitalWrite(red_pin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(red_pin, LOW);
+  delay(10);
+  digitalWrite(blue_pin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(blue_pin, LOW);
+  delay(100);
+}
+void ShortLongPulse(){
+  digitalWrite(red_pin, HIGH);digitalWrite(blue_pin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(red_pin, LOW);
+  delay(10);
+  digitalWrite(blue_pin, LOW);
+  delay(100);
+}
