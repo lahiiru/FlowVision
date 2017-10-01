c5c42782627cc51e557ca95c5b44a4b59d319dd7
YDulanjani <ydulanjani@gmail.com>
2017-10-01 12:14:58 +0530

    new functions added

--- iteration_3/src/algorithms/algorithm.py
+++ iteration_3/src/algorithms/algorithm.py
@@ -2,6 +2,13 @@ class Algorithm:
 
     def __init__(self):
         self.debug = True
+        self.pixels_per_second=0
 
-    def compare(self, current_frame, prev_frame, **kwargs):
+    def get_pixels_per_second(self):
+        return self.pixels_per_second
+
+    def receive_frames(self, frame):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+    def update(self,**kwargs):
         raise NotImplementedError("Subclass must implement abstract method")
