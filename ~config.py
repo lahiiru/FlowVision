4ee6dcd857e88e0881914dba52c3591bc78c1d66
JA Lahiru P Jayakody <lahiiru@gmail.com>
2017-10-02 21:31:36 +0530

    Config template added

---
+++ ~config.py
@@ -0,0 +1,17 @@
+"""
+    Configuration parameters
+    once you pulled this file change as your configuration then exclude from git
+"""
+class DevConfig:
+    BUILD_IN_CAM_INDEX = 
+    BUILD_IN_CAM_F = 
+    WEB_CAM_INDEX = 
+    WEB_CAM_F = 
+    VIDEO_DIR = 
+    TEST_VIDEO = 
+
+class ProdConfig:
+    BUILD_IN_CAM_INDEX = 
+    BUILD_IN_CAM_F = 
+    WEB_CAM_INDEX = 
+    WEB_CAM_F = 
