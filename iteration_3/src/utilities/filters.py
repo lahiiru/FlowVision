262e4e4bb8c0db9c6b4c98e152a57a84660134cc
YDulanjani <ydulanjani@gmail.com>
2017-10-02 23:19:58 +0530

    illumination filter changed

--- iteration_3/src/utilities/filters.py
+++ iteration_3/src/utilities/filters.py
@@ -1,11 +1,10 @@
 import cv2
-import numpy as np
+from cv2 import *
+
 
 class Filters:
-
     background_substractor = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=10, detectShadows=False)
     morphological_opening_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-
 
     def __init__(self):
         pass
@@ -16,10 +15,10 @@ class Filters:
 
     @staticmethod
     def morphological_opening_filter(frame):
-        return cv2.morphologyEx(frame, cv2.MORPH_OPEN,Filters.morphological_opening_kernel)
-
+        return cv2.morphologyEx(frame, cv2.MORPH_OPEN, Filters.morphological_opening_kernel)
 
     @staticmethod
-    def illumiationFilter(original_frame,backSub_frame):
-        illumination_removed_frame = backSub_frame[original_frame>250]=0
-        return illumination_removed_frame
+    def illumination_filter(original_frame, backSub_frame):
+        original_frame = cv2.cvtColor(original_frame, cv2.COLOR_RGB2GRAY)
+        backSub_frame[original_frame > 250] = 0
+        return backSub_frame
