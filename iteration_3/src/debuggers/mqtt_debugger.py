e8be6911332c588cfab576014d69df0f094a85dc
DulajPerera <d.ayeshan@gmail.com>
2017-10-16 13:26:07 +0530

    Removed json conversion

--- iteration_3/src/debuggers/mqtt_debugger.py
+++ iteration_3/src/debuggers/mqtt_debugger.py
@@ -1,14 +1,11 @@
-import json
-
-
 class MQTTDebugger:
 
     def __init__(self):
         pass
 
     @staticmethod
-    def get_state_json(self):
+    def get_state_object(self):
         state = {}
         state['camera'] = self.device.camera.get_state()
         state['algorithm'] = self.device.algorithm.get_state()
-        return json.dumps(state)
+        return state
